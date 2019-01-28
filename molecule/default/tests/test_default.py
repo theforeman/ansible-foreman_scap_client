f698b2ffa8ba9e1e311b05f2f9c73483f94d5216
Ondrej Prazak <oprazak@redhat.com>
2019-01-28 12:43:08 +0100

    Fix syntax errors in tasks

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+# import yaml
 
 import testinfra.utils.ansible_runner
 
@@ -6,9 +7,20 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+# def test_hosts_file(host):
+#     f = host.file('/etc/hosts')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+#     assert f.exists
+#     assert f.user == 'root'
+#     assert f.group == 'root'
+
+def test_foreman_scap_client_config(host):
+    file = host.file('/etc/foreman_scap_client/config.yaml')
+
+    assert file.exists
+
+    # with open('/etc/foreman_scap_client/config.yaml') as stream:
+    #     config = yaml.safe_load(stream)
+
+    # config["foreman_scap_client_port"]
+    # assert len(config["foreman_scap_client"]) == 2
