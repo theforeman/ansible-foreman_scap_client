787f57ef043b94ccb121db69c4fc69a95aa5a136
Ben Formosa <ben.formosa@gmail.com>
2020-12-01 14:05:18 +1100

    add documentation

--- library/foreman_scap_client_facts.py
+++ library/foreman_scap_client_facts.py
@@ -1,5 +1,44 @@
 #!/usr/bin/python
 
+DOCUMENTATION = r"""
+---
+module: foreman_scap_client_facts
+version_added: 2.0.0
+short_description: Gather facts about Red Hat Subscription Manager
+description:
+    - Gathers facts about Red Hat Subscription Manager from rhsm.conf.
+"""
+
+EXAMPLES = r"""
+# Gather facts and register to a variable.
+- name: Gather foreman_scap_client facts
+  foreman_scap_client_facts:
+  register: my_foreman_scap_client_facts
+"""
+
+RETURN = r"""
+paths:
+    description: dictionary containing all the facts
+    returned: success
+    type: complex
+    contains:
+        rh_ca_cert_path:
+            description: Default CA cert to use when generating yum repo configs
+            returned: success
+            type: str
+            sample: /etc/rhsm/ca/katello-server-ca.pem
+        rh_consumer_cert_path
+            description: Path to the consumer's identity certificate
+            returned: success
+            type: str
+            sample: /etc/pki/consumer/cert.pem
+        rh_consumer_key_path
+            description: Path to the key for the consumer's identity certificate
+            returned: success
+            type: str
+            sample: /etc/pki/consumer/key.pem
+"""
+
 import json
 try:
     from ConfigParser import SafeConfigParser
